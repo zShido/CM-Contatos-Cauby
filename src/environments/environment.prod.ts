088fd4f5c91db60f9f78f7efde9c1a38135e70a9
Carlos Eduardo Andrade Iatskiu <carlosiatskiu@MacBook-Pro-de-Carlos.local>
2023-10-31 09:45:04 -0300

    novo apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyAh9uQog2HrqBym3scXouVXNW4hrXLvoCU",
-    authDomain: "app2023contatosta-61518.firebaseapp.com",
-    projectId: "app2023contatosta-61518",
-    storageBucket: "app2023contatosta-61518.appspot.com",
-    messagingSenderId: "206513618836",
-    appId: "1:206513618836:web:824ad1da35e4be306a1981"
+   //inserir aqui sua chave
   }
 };
