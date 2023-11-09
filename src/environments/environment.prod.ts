22d10250efeac4c07c7810cfc41766e18e2647c5
zShido <caubyjeth13@gmail.com>
2023-11-09 10:44:08 -0300

    oi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-   //inserir aqui sua chave
+    apiKey: "AIzaSyD0fdKjyrTH1hwTlJVWFIO_Zz-_gdAGQqQ",
+    authDomain: "listacontatosnova-d0ff1.firebaseapp.com",
+    projectId: "listacontatosnova-d0ff1",
+    storageBucket: "listacontatosnova-d0ff1.appspot.com",
+    messagingSenderId: "53772069585",
+    appId: "1:53772069585:web:faae76826a8aa9bfefb3f0"
   }
 };
