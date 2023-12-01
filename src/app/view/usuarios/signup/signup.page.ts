9561e9dc94736b6fdf828a68df51b49de44e4c27
zShido <caubyjeth13@gmail.com>
2023-12-01 09:20:20 -0300

    Sign In e Sign Up com authenticacao completa

--- src/app/view/usuarios/signup/signup.page.ts
+++ src/app/view/usuarios/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  formCadastrar:FormGroup
 
-  constructor() { }
+  constructor(private alertService : AlertService,
+    private router : Router,
+    private formBuilder: FormBuilder) { 
+      this.formCadastrar = new FormGroup({
+        email: new FormControl(''),
+        senha: new FormControl(''),
+        confSenha: new FormControl('')
+      });
+    }
 
   ngOnInit() {
+    this.formCadastrar = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confSenha: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  get errorControl(){
+    return this.formCadastrar.controls;
+  }
+
+
+  submitForm() : boolean{
+    if(!this.formCadastrar.valid){
+      this.alertService.presentAlert("Erro","Erro ao Preencher os campos!");
+      return false;
+    }else{
+      this.cadastrar();
+      return true;
+    }
+  }
+
+  private cadastrar(){
+    this.alertService.presentAlert("Cadastro","Cadastro realizado com Sucesso!");
+    this.router.navigate(["signin"])
+  }
 }
